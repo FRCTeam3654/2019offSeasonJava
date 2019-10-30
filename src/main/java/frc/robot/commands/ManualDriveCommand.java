76bae08927fb7d65a0b5f8b135e79ef329e24ea5
kkulakowski707 <52632612+kkulakowski707@users.noreply.github.com>
2019-10-30 17:56:15 -0400

    Put mercy Arcade Drive method inside Arcade drive

--- src/main/java/frc/robot/commands/ManualDriveCommand.java
+++ src/main/java/frc/robot/commands/ManualDriveCommand.java
@@ -9,7 +9,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 
