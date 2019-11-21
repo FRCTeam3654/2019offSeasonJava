4cd7f096fc1993fa44592543f34997a6ef45bc81
kkulakowski707 <52632612+kkulakowski707@users.noreply.github.com>
2019-11-20 19:35:07 -0500

    Finished turn 90 Command

--- src/main/java/frc/robot/commands/Turn90DegreesCommand.java
+++ src/main/java/frc/robot/commands/Turn90DegreesCommand.java
@@ -11,8 +11,12 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
+import java.math.*;
 
 public class Turn90DegreesCommand extends Command {
+  private boolean turn90Flag = false;
+  private double turn90Angle = 0;
+  private double vinniesError = 2;//greater than 1 so it doesn't get triggered until the error is correct
   public Turn90DegreesCommand() {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.drive);
@@ -26,15 +30,31 @@ public class Turn90DegreesCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double turn90X;
+    double turn90Y;
     double [] yawPitchRollArray;
     yawPitchRollArray = new double [3];
 
     Robot.drive.pigeonVinnie.getYawPitchRoll(yawPitchRollArray);
+     
+      if (!turn90Flag){
+        turn90Angle = yawPitchRollArray[0] + 90;
+        turn90Flag = true;
+      }
+      vinniesError = turn90Angle - yawPitchRollArray[0];
+       turn90X = vinniesError * RobotMap.turnDegreeProportion;
+       turn90X = Math.min(1, turn90X);
+       turn90X = Math.max(-1, turn90X);
+       turn90Y = 0;
+       Robot.drive.setArcade(turn90X, turn90Y);
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
+    if(Math.abs(vinniesError) < 1){
+      return true;
+    }
     return false;
   }
 
