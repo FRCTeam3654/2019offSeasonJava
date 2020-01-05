384eba7b75b7b11f5e6a7d3fe7fdf492e4537301
Kasznaa <52632578+Kasznaa@users.noreply.github.com>
2020-01-05 17:56:57 -0500

    Comments

---
+++ src/main/java/frc3654/TestMain.java
@@ -0,0 +1,18 @@
+
+//put all team code in this pkg which can be copied to new prject template next season
+package frc3654;
+
+import frc.robot.OI;
+
+public final class TestMain {
+
+    //TODO test class - remove later
+    public static void main(String... args) {
+      System.out.println("test class");
+    }
+
+    public static String test(OI m_oi) {
+        System.out.println("plain test");
+        return null;
+    }
+}
